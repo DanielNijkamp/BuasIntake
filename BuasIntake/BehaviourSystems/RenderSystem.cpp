655bf56de7452fcacbd1b802ae050dbaac804476
Daniel <danielnijkamp2004@gmail.com>
2024-05-15 11:20:54 +0200

    Nuked repository

--- BuasIntake/BehaviourSystems/RenderSystem.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "RenderSystem.h"
-
-void RenderSystem::Render(sf::RenderWindow& window) const
-{
-    for (const auto& renderable : renderables)
-    {
-        renderable->Render(window);
-    }
-}
